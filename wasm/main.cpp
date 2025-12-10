c7e5b9cad8fa8751b47a9d0b755909c040520a60
BayernMuller <cjjunk29@gmail.com>
2025-12-10 21:59:22 +0900

    refactor: update processFrame and drawLandmarks functions to handle multiple faces and adjust landmark processing

--- wasm/main.cpp
+++ wasm/main.cpp
@@ -12,97 +12,102 @@ constexpr std::size_t LOWER_LIP_CENTER_INDEX = 17;
 constexpr std::size_t NORMALIZED_IMAGE_SIZE = 256;
 constexpr std::size_t EYE_DISTANCE = 96;
 
+constexpr std::size_t NUM_FACE_LANDMARKS = 478;
+
 extern "C" {
     void process_frame(
         unsigned char* rgba_image,
         int image_width,
         int image_height,
         const float* face_landmarks,
-        int num_face_landmarks) {
+        int num_faces) {
 
-        std::vector<geo::Point> landmark_points;
-        landmark_points.reserve(num_face_landmarks / 2);
+        for (int face_index = 0; face_index < num_faces; face_index++) {
+            const float* one_face_landmarks = &face_landmarks[face_index * NUM_FACE_LANDMARKS * 2];
 
-        for (std::size_t i = 0; i < num_face_landmarks; i += 2) {
-            landmark_points.push_back(geo::Point{
-                face_landmarks[i] * image_width,
-                face_landmarks[i + 1] * image_height
-            });
+            std::vector<geo::Point> landmark_points;
+            landmark_points.reserve(NUM_FACE_LANDMARKS);
+            
+            for (std::size_t i = 0; i < NUM_FACE_LANDMARKS; i++) {
+                landmark_points.push_back(geo::Point{
+                    one_face_landmarks[i * 2] * image_width,
+                    one_face_landmarks[i * 2 + 1] * image_height
+                });
+            }
+
+            std::vector<geo::Point> left_iris_points;
+            std::vector<geo::Point> right_iris_points;
+            left_iris_points.reserve(LEFT_IRIS_INDICES.size());
+            right_iris_points.reserve(RIGHT_IRIS_INDICES.size());
+
+            for (const auto& index : LEFT_IRIS_INDICES) {
+                left_iris_points.push_back(landmark_points[index]);
+            }
+            geo::Point left_iris_center = geo::mean(left_iris_points);
+
+            for (const auto& index : RIGHT_IRIS_INDICES) {
+                right_iris_points.push_back(landmark_points[index]);
+            }
+            geo::Point right_iris_center = geo::mean(right_iris_points);
+
+            std::vector<geo::Point> src_points = {
+                left_iris_center,
+                right_iris_center,
+            };
+
+            std::vector<geo::Point> dst_points = {
+                {NORMALIZED_IMAGE_SIZE / 2.0 - EYE_DISTANCE / 2.0, 50},
+                {NORMALIZED_IMAGE_SIZE / 2.0 + EYE_DISTANCE / 2.0, 50},
+            };
+
+            float partial_affine_matrix[2][3];
+            bool success = geo::estimateAffinePartial2D(
+                src_points,
+                dst_points,
+                partial_affine_matrix);
+
+            if (!success) {
+                return;
+            }
+
+            auto normalized_points = geo::transformLandmarks(
+                landmark_points,
+                partial_affine_matrix);
+
+            float* input = reinterpret_cast<float*>(normalized_points.data());
+            float output[OUTPUT_DIM];
+            forward(input, output);
+
+            int max_index = std::max_element(output, output + OUTPUT_DIM) - output;
+
+            const unsigned char* emoji[] = {
+                emoji::ANGRY_EMOJI,
+                emoji::HAPPY_EMOJI,
+                emoji::NEUTRAL_EMOJI,
+                emoji::SURPRISE_EMOJI };
+
+            const std::vector<geo::Point>* emoji_points[] = {
+                &emoji::ANGRY_POINTS,
+                &emoji::HAPPY_POINTS,
+                &emoji::NEUTRAL_POINTS,
+                &emoji::SURPRISE_POINTS
+            };
+
+            std::vector<geo::Point> canvas_points = {
+                left_iris_center,
+                right_iris_center,
+                landmark_points[LOWER_LIP_CENTER_INDEX],
+            };
+
+            geo::overlayWarpAffine(
+                rgba_image,
+                image_width,
+                image_height,
+                emoji[max_index],
+                emoji::EMOJI_WIDTH,
+                emoji::EMOJI_HEIGHT,
+                canvas_points,
+                *emoji_points[max_index]);
         }
-
-        std::vector<geo::Point> left_iris_points;
-        std::vector<geo::Point> right_iris_points;
-        left_iris_points.reserve(LEFT_IRIS_INDICES.size());
-        right_iris_points.reserve(RIGHT_IRIS_INDICES.size());
-
-        for (const auto& index : LEFT_IRIS_INDICES) {
-            left_iris_points.push_back(landmark_points[index]);
-        }
-        geo::Point left_iris_center = geo::mean(left_iris_points);
-
-        for (const auto& index : RIGHT_IRIS_INDICES) {
-            right_iris_points.push_back(landmark_points[index]);
-        }
-
-        geo::Point right_iris_center = geo::mean(right_iris_points);
-
-        std::vector<geo::Point> src_points = {
-            left_iris_center,
-            right_iris_center,
-        };
-
-        std::vector<geo::Point> dst_points = {
-            {NORMALIZED_IMAGE_SIZE / 2.0 - EYE_DISTANCE / 2.0, 50},
-            {NORMALIZED_IMAGE_SIZE / 2.0 + EYE_DISTANCE / 2.0, 50},
-        };
-
-        float partial_affine_matrix[2][3];
-        bool success = geo::estimateAffinePartial2D(
-            src_points,
-            dst_points,
-            partial_affine_matrix);
-
-        if (!success) {
-            return;
-        }
-
-        auto normalized_points = geo::transformLandmarks(
-            landmark_points,
-            partial_affine_matrix);
-
-        float* input = reinterpret_cast<float*>(normalized_points.data());
-        float output[OUTPUT_DIM];
-        forward(input, output);
-
-        int max_index = std::max_element(output, output + OUTPUT_DIM) - output;
-
-        const unsigned char* emoji[] = {
-            emoji::ANGRY_EMOJI,
-            emoji::HAPPY_EMOJI,
-            emoji::NEUTRAL_EMOJI,
-            emoji::SURPRISE_EMOJI };
-
-        const std::vector<geo::Point>* emoji_points[] = {
-            &emoji::ANGRY_POINTS,
-            &emoji::HAPPY_POINTS,
-            &emoji::NEUTRAL_POINTS,
-            &emoji::SURPRISE_POINTS
-        };
-
-        std::vector<geo::Point> canvas_points = {
-            left_iris_center,
-            right_iris_center,
-            landmark_points[LOWER_LIP_CENTER_INDEX],
-        };
-
-        geo::overlayWarpAffine(
-            rgba_image,
-            image_width,
-            image_height,
-            emoji[max_index],
-            emoji::EMOJI_WIDTH,
-            emoji::EMOJI_HEIGHT,
-            canvas_points,
-            *emoji_points[max_index]);
     }
 }
